b178e410cc8efdc6f6322ee543cf7853b9db96c0
shenpeng <396635614@qq.com>
2018-12-02 20:16:21 +0800

    feat：增加next-demo文件

---
+++ examples/next-demo/pages/index.js
@@ -0,0 +1,57 @@
+import Layout from '../components/Layout'
+import { Button } from 'antd-mobile'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = props => (
+  <Layout title="Index">
+    <ul>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <Link href="/about">
+      <Button>Go to About</Button>
+    </Link>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`服务端获取数据！`)
+
+  return {
+    shows: data
+  }
+}
+
+export default Index
